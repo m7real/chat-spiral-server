bcb23238c110527f17a555e0de20afb287148e1c
m7real <masayeedreal@gmail.com>
2022-12-27 21:35:01 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
@@ -10,6 +11,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// mongoDB
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wo3xvdt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    const userCollection = client.db("chatSpiral").collection("users");
+
+    // database CRUD goes here
+  } finally {
+        // prettier-ignore
+    }
+}
+
+run().catch((err) => console.error(err));
+
+// test APIs
 app.get("/", (req, res) => {
   res.send("Chat Spiral Server Running");
 });
